427ca940338db9591d1433ec6e15ae07b0991c3d
martinahristoska <martinahristoska999@hotmail.com>
2021-07-01 21:57:00 +0200

    file added

---
+++ CinemaTicketsAdminApplication/Models/CinemaTicketsApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaTicketsAdminApplication.Models
+{
+    public class CinemaTicketsApplicationUser
+    {
+        public string Email { get; set; }
+
+        public string NormalizedUserName { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ CinemaTicketsAdminApplication/Models/TicketInOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaTicketsAdminApplication.Models
+{
+    public class TicketInOrder
+    {
+        public Guid TicketId { get; set; }
+        public Ticket SelectedTicket { get; set; }
+        public Guid OrderId { get; set; }
+        public Order UserOrder { get; set; }
+        public int Quantity { get; set; }
+    }
+}
